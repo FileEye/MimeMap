ee1905e6891706b5fd887a0a636983c4dcd24725
mondrake <mondrake.org@gmail.com>
2019-01-16 12:15:13 +0100

    7

--- tests/ExtensionTest.php
+++ tests/ExtensionTest.php
@@ -3,8 +3,6 @@
 namespace FileEye\MimeMap\test;
 
 use FileEye\MimeMap\Extension;
-use FileEye\MimeMap\MapHandler;
-use FileEye\MimeMap\Type;
 use PHPUnit\Framework\TestCase;
 
 class ExtensionTest extends TestCase
@@ -47,47 +45,5 @@ class ExtensionTest extends TestCase
     public function testGetNoStrictTypesUnknownExtension()
     {
         $this->assertSame(['application/octet-stream'], (new Extension('ohmygodthatisnoextension'))->getTypes(false));
-    }
-
-    // xx move to its own test
-
-    public function testAddMapping()
-    {
-        $map = new MapHandler();
-        $map->addMapping('bingo/bongo', 'bngbng');
-        $this->assertSame('bingo/bongo', (new Extension('bngbng'))->getDefaultType());
-        $this->assertSame(['bingo/bongo'], (new Extension('bngbng'))->getTypes());
-
-        // Adding an already existing mapping should not duplicate entries.
-        $map->addMapping('bingo/bongo', 'bngbng');
-        $this->assertSame(['bingo/bongo'], (new Extension('bngbng'))->getTypes());
-    }
-
-    public function testRemoveMapping()
-    {
-        $map = new MapHandler();
-        $this->assertSame('text/plain', (new Extension('txt'))->getDefaultType());
-        $map->removeMapping('text/plain', 'txt');
-        $this->assertSame('application/octet-stream', (new Extension('txt'))->getDefaultType(false));
-        $this->assertSame('text/plain', (new Extension('text'))->getDefaultType());
-        $map->removeType('text/plain');
-        $this->assertSame(['application/octet-stream'], (new Extension('text'))->getTypes(false));
-        $this->assertSame('application/octet-stream', (new Extension('text'))->getDefaultType(false));
-    }
-
-    public function testSetExtensionDefaultType()
-    {
-        $map = new MapHandler();
-        $this->assertSame(['image/vnd.dvb.subtitle', 'text/vnd.dvb.subtitle'], (new Extension('sub'))->getTypes());
-        $map->setExtensionDefaultType('SUB', 'text/vnd.dvb.subtitle');
-        $this->assertSame(['text/vnd.dvb.subtitle', 'image/vnd.dvb.subtitle'], (new Extension('SUB'))->getTypes());
-    }
-
-    public function testSetTypeDefaultExtension()
-    {
-        $map = new MapHandler();
-        $this->assertSame(['jpeg', 'jpg', 'jpe'], (new Type('image/jpeg'))->getExtensions());
-        $map->setTypeDefaultExtension('image/jpeg', 'jpg');
-        $this->assertSame(['jpg', 'jpeg', 'jpe'], (new Type('image/JPEG'))->getExtensions());
     }
 }
---
+++ tests/MapHandlerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace FileEye\MimeMap\test;
+
+use FileEye\MimeMap\Extension;
+use FileEye\MimeMap\MapHandler;
+use FileEye\MimeMap\Type;
+use PHPUnit\Framework\TestCase;
+
+class MapHandlerTest extends TestCase
+{
+    public function testAdd()
+    {
+        $map = new MapHandler();
+        $map->addMapping('bingo/bongo', 'bngbng');
+        $this->assertSame(['bngbng'], (new Type('bingo/bongo'))->getExtensions());
+        $this->assertSame('bngbng', (new Type('bingo/bongo'))->getDefaultExtension());
+        $this->assertSame(['bingo/bongo'], (new Extension('bngbng'))->getTypes());
+        $this->assertSame('bingo/bongo', (new Extension('bngbng'))->getDefaultType());
+
+        // Adding an already existing mapping should not duplicate entries.
+        $map->addMapping('bingo/bongo', 'bngbng');
+        $this->assertSame('[bngbng]', (new Type('bingo/bongo'))->getExtensions());
+        $this->assertSame(['bingo/bongo'], (new Extension('bngbng'))->getTypes());
+    }
+
+    public function testRemove()
+    {
+        $map = new MapHandler();
+
+        $this->assertSame(['txt', 'text', 'conf', 'def', 'list', 'log', 'in'], (new Type('text/plain'))->getExtensions());
+        $this->assertSame('txt', (new Type('text/plain'))->getDefaultExtension());
+        $this->assertSame(['text/plain'], (new Extension('txt'))->getTypes());
+        $this->assertSame('text/plain', (new Extension('txt'))->getDefaultType());
+
+        $map->removeMapping('text/plain', 'txt');
+
+        $this->assertSame(['text', 'conf', 'def', 'list', 'log', 'in'], (new Type('text/plain'))->getExtensions());
+        $this->assertSame('text', (new Type('text/plain'))->getDefaultExtension());
+        $this->assertSame(['application/octet-stream'], (new Extension('txt'))->getTypes(false));
+        $this->assertSame('application/octet-stream', (new Extension('txt'))->getDefaultType(false));
+
+        $map->removeType('text/plain');
+
+        $this->assertSame(['txt'], (new Type('text/plain'))->getExtensions(false));
+        $this->assertSame('txt', (new Type('text/plain'))->getDefaultExtension(false));
+        $this->assertSame(['application/octet-stream'], (new Extension('txt'))->getTypes(false));
+        $this->assertSame('text/plain', (new Extension('txt'))->getDefaultType(false));
+    }
+
+    public function testSetExtensionDefaultType()
+    {
+        $map = new MapHandler();
+        $this->assertSame(['image/vnd.dvb.subtitle', 'text/vnd.dvb.subtitle'], (new Extension('sub'))->getTypes());
+        $map->setExtensionDefaultType('SUB', 'text/vnd.dvb.subtitle');
+        $this->assertSame(['text/vnd.dvb.subtitle', 'image/vnd.dvb.subtitle'], (new Extension('SUB'))->getTypes());
+    }
+
+    public function testSetTypeDefaultExtension()
+    {
+        $map = new MapHandler();
+        $this->assertSame(['jpeg', 'jpg', 'jpe'], (new Type('image/jpeg'))->getExtensions());
+        $map->setTypeDefaultExtension('image/jpeg', 'jpg');
+        $this->assertSame(['jpg', 'jpeg', 'jpe'], (new Type('image/JPEG'))->getExtensions());
+    }
+}
