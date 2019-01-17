a39b890baea1953c97fb409351b1dad1dd00db5f
mondrake <mondrake.org@gmail.com>
2019-01-17 18:13:07 +0100

    2

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -3,7 +3,7 @@
 namespace FileEye\MimeMap\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
