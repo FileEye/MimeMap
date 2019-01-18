523b42ca7d69299bb7f35cd25d9eb04bd2ee6763
mondrake <mondrake.org@gmail.com>
2019-01-18 18:19:19 +0100

    x7

---
+++ src/EmptyMap.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FileEye\MimeMap;
+
+/**
+ * Class for mapping file extensions to MIME types.
+ *
+ * This class has no mapping defined. It can be used for feeding updates and
+ * for testing purposes.
+ */
+class EmptyMap extends AbstractMap
+{
+    /**
+     * Mapping between file extensions and MIME types.
+     *
+     * Intentionally left blank.
+     *
+     * @internal
+     *
+     * @var array
+     */
+    protected static $map = [];
+}
