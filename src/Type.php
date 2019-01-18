5337c06c1d4dc4d9922e0ee03995b3f047b2fea8
mondrake <mondrake.org@gmail.com>
2019-01-18 13:47:14 +0100

    3

--- src/Type.php
+++ src/Type.php
@@ -279,7 +279,10 @@ class Type
             return false;
         }
 
-        $wildcard_re = str_replace('*', '.*', $wildcard_type->toString(static::SHORT_TEXT));
+        $wildcard_re = strtr($wildcard_type->toString(static::SHORT_TEXT), [
+            '/' => '\\/',
+            '*' => '.*',
+        ]);
         $subject = $wildcard_type->toString(static::SHORT_TEXT);
 dump([$wildcard_re, $subject]);
 
