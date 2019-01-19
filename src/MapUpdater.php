51e320d0dfe4f6d3d6cf81a7cab8c3e020eee113
mondrake <mondrake.org@gmail.com>
2019-01-19 11:26:35 +0100

    14

--- src/MapUpdater.php
+++ src/MapUpdater.php
@@ -47,6 +47,8 @@ class MapUpdater
     public function createMapFromSourceFile($source_file = MapUpdater::DEFAULT_SOURCE_FILE)
     {
         $map = MapHandler::map('\FileEye\MimeMap\Map\EmptyMap');
+dump($map);
+return $map;
         $lines = file($source_file);
         foreach ($lines as $line) {
             if ($line{0} == '#') {
