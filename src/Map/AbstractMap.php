36c627e4d154edbb328c0ea2177c9218b626ce5c
mondrake <mondrake.org@gmail.com>
2019-03-19 18:03:43 +0100

    fin003

--- src/Map/AbstractMap.php
+++ src/Map/AbstractMap.php
@@ -305,7 +305,7 @@ abstract class AbstractMap extends BaseMap
             $extension_ret = $this->removeMapSubEntry('e', $extension, 't', $alias);
         } else {
             // Remove any associated aliased type as well.
-            if ($associated_types = $this->getMapSubEntry('e', $extension, 't')) {
+            if ($associated_types = $this->getExtensionTypes($extension)) {
                 foreach ($associated_types as $associated_type) {
                     if ($this->hasAlias($associated_type) && $type === $this->getAliasTypes($associated_type)[0]) {
                         $this->removeMapSubEntry('a', $associated_type, 'e', $extension);
