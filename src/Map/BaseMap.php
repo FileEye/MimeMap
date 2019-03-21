bac56a8ec3c3cd9b3ff644106839c191918d6585
mondrake <mondrake.org@gmail.com>
2019-03-21 17:50:41 +0100

    4

--- src/Map/BaseMap.php
+++ src/Map/BaseMap.php
@@ -12,6 +12,13 @@ use FileEye\MimeMap\MappingException;
 abstract class BaseMap
 {
     /**
+     * Mapping between file extensions and MIME types.
+     *
+     * @var array
+     */
+    protected static $map = [];
+
+    /**
      * A backup of the mapping between file extensions and MIME types.
      *
      * Used during the map update process.
