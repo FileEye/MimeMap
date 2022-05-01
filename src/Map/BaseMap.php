ec831cc16365bd9d6578c0005909ba73031ed1c0
mondrake <mondrake.org@gmail.com>
2022-05-01 18:40:38 +0200

    Fix

--- src/Map/BaseMap.php
+++ src/Map/BaseMap.php
@@ -21,7 +21,7 @@ abstract class BaseMap implements MapInterface
     /**
      * Mapping between file extensions and MIME types.
      *
-     * @var array<string,array<int|string,array<int,string>>>
+     * @var array<string, array<int|string, array<string, array<int,string>>>>
      */
     protected static $map = [];
 
@@ -30,7 +30,7 @@ abstract class BaseMap implements MapInterface
      *
      * Used during the map update process.
      *
-     * @var array<string,array<int|string,array<int,string>>>|null
+     * @var array<string, array<int|string, array<string, array<int,string>>>>
      */
     protected static $backupMap;
 
